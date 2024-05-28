ed09902e8651dcea098df1890a08a443fc0c4d40
filipemedeiross <filipemedeiros2013@gmail.com>
2024-05-28 16:41:50 -0300

    Improving the interface with the "obstacle" submodule

--- dino_game/obstacle.py
+++ dino_game/obstacle.py
@@ -1,49 +1,65 @@
 from pygame.sprite import Sprite
 from pygame.mask import from_surface
-from .constants import SCREEN_WDTH, SPEED_GAME, SPEED_ANIMATION
+from .constants import SPEED_GAME,      \
+                       SPEED_ANIMATION, \
+                       SCREEN_WDTH
 
 
 class SingleObstacle(Sprite):
-    WIDTH = SCREEN_WDTH
     SPEED = SPEED_GAME
+    WIDTH = SCREEN_WDTH
 
     def __init__(self, image, y):
         Sprite.__init__(self)
 
         self.image = image
-        self.mask  = from_surface(self.image)
+        self.mask  = from_surface(image)
         self.rect  = self.image.get_rect(topleft=(self.WIDTH, y))
 
     def update(self):
         self.rect.x -= self.SPEED
-        if self.rect.x < -self.rect.width:
+
+        if self.rect.right < 0:
             self.kill()
 
 
 class MultiObstacle(Sprite):
-    WIDTH = SCREEN_WDTH
     SPEED = SPEED_GAME
-    TIME_ANIMATION = SPEED_ANIMATION
+    WIDTH = SCREEN_WDTH
+    TIME  = SPEED_ANIMATION
 
     def __init__(self, images, y):
         Sprite.__init__(self)
 
-        self.imgs = images
-        self.msks = [from_surface(img) for img in self.imgs]
-
-        self.idx = 0
-        self.m_idx = len(self.imgs) * self.TIME_ANIMATION
-
-        self.image = self.imgs[self.idx]
-        self.mask  = self.msks[self.idx]
-        self.rect  = self.image.get_rect(topleft=(self.WIDTH, y))
+        self.init_sprites(images, y)
 
     def update(self):
-        self.idx = (self.idx + 1) % self.m_idx
-        self.image = self.imgs[self.idx // self.TIME_ANIMATION]
-        self.mask  = self.msks[self.idx // self.TIME_ANIMATION]
-        self.rect  = self.image.get_rect(topleft=self.rect.topleft)
+        self.update_index()
+        self.update_sprite(self.idx // self.TIME, self.topleft)
 
         self.rect.x -= self.SPEED
-        if self.rect.x < -self.rect.width:
+
+        if self.rect.right < 0:
             self.kill()
+
+    def init_sprites(self, images, y):
+        self.images = images
+        self.masks  = [from_surface(image)
+                       for image in self.images]
+
+        self.idx = 0
+        self.max = len(self.images) * self.TIME
+
+        self.update_sprite(self.idx, (self.WIDTH, y))
+
+    def update_index(self):
+        self.idx = (self.idx + 1) % self.max
+
+    def update_sprite(self, idx, topleft):
+        self.image = self.images[idx]
+        self.mask  = self.masks [idx]
+        self.rect  = self.image.get_rect(topleft=topleft)
+
+    @property
+    def topleft(self):
+        return self.rect.topleft
