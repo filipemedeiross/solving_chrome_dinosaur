01c5c45f9f80dd396d5d4645b4b63f3730546c29
filipemedeiross <filipemedeiros2013@gmail.com>
2024-05-28 15:30:16 -0300

    Improving the interface with the "cloud" class

--- dino_game/cloud.py
+++ dino_game/cloud.py
@@ -1,24 +1,24 @@
 from random import randint
 from pygame.image import load
-from pygame.mask  import from_surface
-from .constants import SCREEN_WDTH, CLOUD_SPEED, \
+from .constants import CLOUD_PATH , CLOUD_SPEED, \
                        CLOUD_MIN_X, CLOUD_MAX_X, \
                        CLOUD_MIN_Y, CLOUD_MAX_Y, \
-                       CLOUD_PATH
+                       SCREEN_WDTH
 
 
 class Cloud:
+    PATH  = CLOUD_PATH
+    SPEED = CLOUD_SPEED
     MIN_X = CLOUD_MIN_X
     MAX_X = CLOUD_MAX_X
     MIN_Y = CLOUD_MIN_Y
     MAX_Y = CLOUD_MAX_Y
     WIDTH = SCREEN_WDTH
-    SPEED = CLOUD_SPEED
-    PATH_SPRITE = CLOUD_PATH
 
     def __init__(self):
         self.init_coords()
-        self.image = load(self.PATH_SPRITE)
+
+        self.image = load(self.PATH)
         self.width = self.image.get_width()
 
     def init_coords(self):
@@ -27,8 +27,9 @@ class Cloud:
 
     def update(self):
         self.x -= self.SPEED
-        if self.x < -self.width:
+
+        if self.x + self.width < 0:
             self.init_coords()
 
-    def draw(self, screen):
-        screen.blit(self.image, (self.x, self.y))
+    def draw(self, surface):
+        surface.blit(self.image, (self.x, self.y))
