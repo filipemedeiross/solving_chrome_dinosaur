60f467d415b3d28635104fa7eb5dcbac6f557e32
filipemedeiross <filipemedeiros2013@gmail.com>
2024-05-29 13:44:03 -0300

    Refactoring the play_theme, play_lose_effect, load_agent, predict, get_features and is_cactus methods of the "DinoChrome" class

--- dino_game/chrome_dino.py
+++ dino_game/chrome_dino.py
@@ -1,10 +1,14 @@
-import random
-import pickle
 import pygame
-from .dinosaur import Dinosaur
-from .cloud import Cloud
-from .base import Base
-from .enemies import SmallCactus, LargeCactus, Bird
+from pickle import load
+from random import choice, \
+                   randint
+
+from .base      import Base
+from .cloud     import Cloud
+from .dinosaur  import Dinosaur
+from .enemies   import SmallCactus, \
+                       LargeCactus, \
+                       Bird
 from .constants import *
 
 
@@ -13,13 +17,13 @@ class DinoChrome:
         # Initializing the pygame
         pygame.init()
 
-        # Screen is initialized with init_game method
-        self.screen = None
+        # Init the screen, clock, font and mixer
+        self.screen = pygame.display.set_mode(SIZE)
+        self.clock  = pygame.time.Clock()
+        self.font   = pygame.font.Font(FONT_TYPE, FONT_SIZE)
 
-        # Init the clock, font and mixer
-        self.clock = pygame.time.Clock()
-        self.font  = pygame.font.Font(FONT_TYPE, FONT_SIZE)
-        
+        pygame.display.set_caption('Dino Chrome')
+
         self.music_game = pygame.mixer.Sound(GAME_THEME_PATH)
         self.music_lose = pygame.mixer.Sound(GAME_EFFCT_PATH)
 
@@ -30,21 +34,17 @@ class DinoChrome:
         self.score = 0
         self.agent = self.load_agent()
 
-        self.dino  = Dinosaur()
         self.cloud = Cloud()
+        self.dino  = Dinosaur()
         self.base  = Base(BASE_Y)
         self.obstacles = pygame.sprite.Group()
 
     def init_game(self):
-        # Creating a display Surface
-        self.screen = pygame.display.set_mode(SCREEN_SIZE)
-        pygame.display.set_caption('Dino Chrome')
-
         while True:
             self.main_screen()
             self.play()
 
-    def main_screen(self):
+    def main_screen(self):#
         self.play_theme()
 
         while True:
@@ -52,9 +52,9 @@ class DinoChrome:
 
             # Getting input from user
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+                if event.type == QUIT:
                     exit(0)
-                if event.type == pygame.KEYDOWN:
+                if event.type == KEYDOWN:
                     return            
 
             self.dino.update(self.get_features())
@@ -62,14 +62,14 @@ class DinoChrome:
 
             self.display_main_screen()
 
-    def play(self):
+    def play(self):#
         self.score = 0
 
         while True:
             self.clock.tick(FRAMERATE)
 
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+                if event.type == QUIT:
                     exit(0)
 
             self.dino.update(pygame.key.get_pressed())
@@ -82,7 +82,7 @@ class DinoChrome:
                 pygame.time.delay(DELAY)
                 return
 
-    def display_main_screen(self):
+    def display_main_screen(self):#
         self.screen.fill(WHITE)
 
         self.draw_objects()
@@ -90,7 +90,7 @@ class DinoChrome:
 
         pygame.display.flip()
 
-    def display_play_screen(self):
+    def display_play_screen(self):#
         self.screen.fill(WHITE)
 
         self.draw_objects()
@@ -98,7 +98,7 @@ class DinoChrome:
 
         pygame.display.flip()
 
-    def display_text(self):
+    def display_text(self):#
         text = self.font.render('Press any Key to Start', True, BLACK)    
         text_rect = text.get_rect(center=TEXT_INDENT)
         self.screen.blit(text, text_rect)
@@ -108,16 +108,16 @@ class DinoChrome:
             score_rect = score.get_rect(center=(SCORE_MAIN_INDENT))
             self.screen.blit(score, score_rect)
 
-    def display_score(self):
+    def display_score(self):#
         self.score += 1
 
         text = self.font.render(f'Score: {self.score}', True, BLACK)
         text_rect = text.get_rect(center=SCORE_PLAY_INDENT)
         self.screen.blit(text, text_rect)
 
-    def update_obstacles(self):
+    def update_obstacles(self):#
         if not self.obstacles:
-            op = random.randint(0, 2)
+            op = randint(0, 2)
             if op == 0:
                 self.obstacles.add(SmallCactus())
             elif op == 1:
@@ -125,20 +125,20 @@ class DinoChrome:
             elif op == 2:
                 self.obstacles.add(Bird())
 
-    def move_objects(self):
+    def move_objects(self):#
         self.cloud.update()
         self.base.update()
         self.update_obstacles()
         self.obstacles.update()
 
-    def draw_objects(self):
+    def draw_objects(self):#
         self.base.draw(self.screen)
         self.cloud.draw(self.screen)
         self.obstacles.draw(self.screen)
         self.dino.draw(self.screen)
 
     @staticmethod
-    def collide(sprite, group):
+    def collide(sprite, group):#
         return pygame.sprite.spritecollideany(sprite, group, collided=pygame.sprite.collide_mask)
 
     def play_theme(self):
@@ -150,28 +150,30 @@ class DinoChrome:
         self.channel_effct.play(self.music_lose)
 
     def load_agent(self):
-        path = random.choice([DECISION_TREE_PATH,
-                              SVM_LINEAR_PATH])
+        path = choice([TREE_PATH, SVM_PATH])
 
         with open(path, 'rb') as f:
-            agent = pickle.load(f)
+            return load(f)
 
-        return agent
+    def predict(self, x, obs):
+        return self.agent.predict([[x, obs]])[0]
 
     def get_features(self):
-        agent_input = {pygame.K_UP   : False,
-                       pygame.K_DOWN : False}
+        features = {K_UP   : False,
+                    K_DOWN : False}
 
         if self.obstacles:
             obs  = self.obstacles.sprites()[0]
-            pred = self.predict(obs.rect.x - self.dino.rect.right,
-                                0 if isinstance(obs, Bird) else 1)
+            pred = self.predict(obs.x - self.dino.right,
+                                self.is_cactus(obs))
+
             if pred == 1:
-                agent_input[pygame.K_UP] = True
+                features[K_UP  ] = True
             elif pred == 2:
-                agent_input[pygame.K_DOWN] = True
+                features[K_DOWN] = True
 
-        return agent_input
+        return features
 
-    def predict(self, x, obstacle_type):
-        return self.agent.predict([[x, obstacle_type]])[0]
+    @staticmethod
+    def is_cactus(obs):
+        return 0 if isinstance(obs, Bird) else 1
